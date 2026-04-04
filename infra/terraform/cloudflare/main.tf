8a3f3a553f1bb049dbdc5bf61fd7442cd9d489ff
keitaroooo <keitaro.cryptocurrency@gmail.com>
2026-04-04 20:31:40 +0900

    docs: update DNS record ID snapshot and Bitwarden token management feat: update Cloudflare Pages resource to support provider v5 fix: resolve Cloudflare Pages Value Conversion Error

---
+++ infra/terraform/cloudflare/main.tf
@@ -0,0 +1,103 @@
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
+
+# Records are based on the Cloudflare zone export (2026-02-15).
+
+resource "cloudflare_dns_record" "apex_a" {
+  zone_id  = var.zone_id
+  name     = "@"
+  type     = "A"
+  ttl      = 1
+  content  = var.apex_a_value
+  proxied  = false
+}
+
+resource "cloudflare_dns_record" "blog" {
+  zone_id  = var.zone_id
+  name     = "blog"
+  type     = "CNAME"
+  ttl      = 1
+  content  = var.blog_netlify_target
+  proxied  = true
+}
+
+resource "cloudflare_dns_record" "techblog" {
+  zone_id  = var.zone_id
+  name     = "techblog"
+  type     = "CNAME"
+  ttl      = 1
+  content  = var.techblog_vercel_target
+  proxied  = true
+}
+
+resource "cloudflare_dns_record" "traefik" {
+  zone_id  = var.zone_id
+  name     = "traefik"
+  type     = "CNAME"
+  ttl      = 1
+  content  = "keitaroooo.com"
+  proxied  = false
+}
+
+resource "cloudflare_dns_record" "www" {
+  zone_id  = var.zone_id
+  name     = "www"
+  type     = "CNAME"
+  ttl      = 1
+  content  = var.www_pages_target
+  proxied  = true
+}
+
+resource "cloudflare_dns_record" "google_site_verification" {
+  zone_id = var.zone_id
+  name    = "@"
+  type    = "TXT"
+  ttl     = 1
+  content = var.google_site_verification
+}
+
+# Cloudflare Pages Project
+# 既存プロジェクトを import する場合:
+# terraform import cloudflare_pages_project.portfolio <account_id>/portfolio
+
+resource "cloudflare_pages_project" "portfolio" {
+  account_id        = var.cloudflare_account_id
+  name              = "portfolio"
+  production_branch = "main"
+
+  source = {
+    type = "github"
+    config = {
+      owner                         = "keitaroooo"
+      repo_name                     = "portfolio"
+      production_branch             = "main"
+      production_deployments_enabled = true
+      preview_deployment_setting    = "all"
+    }
+  }
+
+  build_config = {
+    build_command   = ""
+    destination_dir = ""
+    root_dir        = ""
+  }
+
+  deployment_configs = {
+    preview = {
+      build_image_major_version = 3
+    }
+    production = {
+      build_image_major_version = 3
+      # 環境変数が必要になった場合はここに追加
+    }
+  }
+}
+
+# Custom domain for Pages
+resource "cloudflare_pages_domain" "www" {
+  account_id   = var.cloudflare_account_id
+  project_name = cloudflare_pages_project.portfolio.name
+  name         = "www.keitaroooo.com"
+}
+
